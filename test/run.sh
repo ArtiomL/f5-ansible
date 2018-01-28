20eb11e02353869a3df00b2529a49308533d2f3e
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2018-01-28 23:26:33 +0200

    docker run

--- test/run.sh
+++ test/run.sh
@@ -3,7 +3,6 @@ set -xe
 set -o pipefail
 
 REPO="artioml/f5-ansible"
-HOST=$(cat inventory/hosts | sed -n '2{p;q}')
 
-docker run -u0 $REPO /bin/sh -c 'rm -f ansible.cfg; ansible-playbook -i inventory/hosts playbooks/app.yml --list-hosts | grep $HOST'
-docker run -u0 $REPO:dev /bin/sh -c 'rm -f ansible.cfg; ansible-playbook -i inventory/hosts playbooks/app.yml --list-hosts | grep $HOST'
+docker run -u0 $REPO /bin/sh -c 'rm -f ansible.cfg; ansible-playbook -i inventory/hosts playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | sed -n 2p)'
+docker run -u0 $REPO:dev /bin/sh -c 'rm -f ansible.cfg; ansible-playbook -i inventory/hosts playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | sed -n 2p)'
