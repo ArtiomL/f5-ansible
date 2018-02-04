48c46c2b19a9dadd3bc990e0ee7515ab2d119a03
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2018-02-04 22:59:05 +0200

    Docker Wrapper Script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,11 @@
 # f5-ansible - Docker Wrapper Script
 # https://github.com/ArtiomL/f5-ansible
 # Artiom Lichtenstein
-# v1.0.0, 04/02/2018
+# v1.0.1, 04/02/2018
 
 if [[ ! -z "$REPO" ]]; then
 	git clone "https://github.com/$REPO.git"
+	cd $(echo "$REPO" | cut -d"/" -f2)
 fi
 
 exec /bin/sh
