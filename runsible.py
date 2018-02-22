df675dccb200751c41831c503bd1a9ccfe1fbf33
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2018-02-22 19:28:03 +0200

    Run Playbooks

--- runsible.py
+++ runsible.py
@@ -18,6 +18,8 @@ def funArgParser():
 		description = 'Run Ansible playbooks, executing the defined tasks on targeted hosts',
 		epilog = 'https://github.com/ArtiomL/f5-ansible')
 	objArgParser.add_argument('-d', '--deploy', help ='deploy a playbook (default)', action = 'store_true')
+	objArgParser.add_argument('-i', help ='service (VS) IP address', dest = 'ip')
+	objArgParser.add_argument('-n', help ='service template (iApp) name', dest = 'name')
 	objArgParser.add_argument('-t', '--teardown', help ='teardown a playbook state', action = 'store_true')
 	objArgParser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
 	objArgParser.add_argument('PLAYBOOK', help = 'playbook name (default: app)', nargs = '?', default = 'app')
@@ -32,6 +34,10 @@ def main():
 		strParams += '-e state="absent" '
 	if objArgs.verbose:
 		strParams += '-vvv '
+	if objArgs.ip:
+		strParams += '-e service_ip="%s" ' % objArgs.ip
+	if objArgs.name:
+		strParams += '-e service_name="%s" ' % objArgs.name
 	strCmd = 'ansible-playbook %s -e @creds.yml --ask-vault-pass %s' % (strPlay, strParams)
 	subprocess.call(strCmd, shell = True)
 
