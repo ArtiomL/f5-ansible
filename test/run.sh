5eb6b8f61fe90b8559a4338933f13a6ecda2be16
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2018-03-05 17:59:29 +0200

    Linting and Check Mode

--- test/run.sh
+++ test/run.sh
@@ -2,12 +2,24 @@
 # f5-ansible - Run Tests
 # https://github.com/ArtiomL/f5-ansible
 # Artiom Lichtenstein
-# v1.0.1, 04/02/2018
+# v1.0.2, 05/03/2018
 
 set -xe
 set -o pipefail
 
 REPO="artioml/f5-ansible"
 
-docker run $REPO /bin/sh -c 'ansible-playbook playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | sed -n 2p)'
-docker run $REPO:dev /bin/sh -c 'ansible-playbook playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | sed -n 2p)'
+# Linting
+for file in */*.yml; do echo "$file"; python -c 'import yaml,sys; yaml.safe_load(sys.stdin)' < "$file"; done
+
+# Ansible check mode
+str_TEST="ansible-playbook playbooks/*app.yml --syntax-check;"
+str_TEST="$str_TEST ansible-playbook playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | sed -n 2p);"
+str_TEST="$str_TEST ansible-playbook playbooks/iapp.yml -e @creds.yml --vault-password-file .password --check;"
+
+if [ "$TRAVIS" == "true" ]; then
+	docker run $REPO /bin/sh -c "$str_TEST"
+	docker run $REPO:dev /bin/sh -c "$str_TEST"
+else
+	eval "$str_TEST"
+fi
