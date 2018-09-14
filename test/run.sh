a6ee0c1d14fcdaeaf4e765a095bcfcfa7a6d8742
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-15 01:47:45 +0300

    Linting and Check Mode

--- test/run.sh
+++ test/run.sh
@@ -13,7 +13,7 @@ for file in */*.yml; do echo "$file"; python -c 'import yaml,sys; yaml.safe_load
 
 # Ansible check mode
 str_TEST="ansible-playbook playbooks/*app.yml --syntax-check;"
-str_TEST="$str_TEST ansible-playbook playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | sed -n 2p);"
+str_TEST="$str_TEST ansible-playbook playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | awk '/bigips/{getline; print}');"
 str_TEST="$str_TEST touch playbooks/f5.http.yml;"
 str_TEST="$str_TEST ansible-playbook playbooks/iapp.yml -e @creds.yml --vault-password-file .password --check;"
 
