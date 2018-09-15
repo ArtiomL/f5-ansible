e7b98c9db05f46295de3ef893d5e27e23d342197
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-16 02:23:39 +0300

    Linting and Check Mode

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@
 # f5-ansible - Run Tests
 # https://github.com/ArtiomL/f5-ansible
 # Artiom Lichtenstein
-# v1.0.3, 21/03/2018
+# v1.0.5, 16/09/2018
 
 set -xeo pipefail
 
@@ -12,10 +12,12 @@ REPO="artioml/f5-ansible"
 for file in */*.yml; do echo "$file"; python -c 'import yaml,sys; yaml.safe_load(sys.stdin)' < "$file"; done
 
 # Ansible check mode
-str_TEST="ansible-playbook playbooks/*app.yml --syntax-check;"
-str_TEST="$str_TEST ansible-playbook playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | awk '/bigips/{getline; print}');"
-str_TEST="$str_TEST touch playbooks/f5.http.yml;"
-str_TEST="$str_TEST ansible-playbook playbooks/iapp.yml -e @creds.yml --vault-password-file .password --check;"
+str_TEST="ansible --version; \
+	ansible-playbook playbooks/*app.yml playbooks/cmd.yml playbooks/pmem.yml --syntax-check; \
+	ansible-playbook playbooks/app.yml --list-hosts | grep $(cat inventory/hosts | awk '/bigips/{getline; print}'); \
+	touch playbooks/f5.http.yml; \
+	ansible-playbook playbooks/iapp.yml -e @creds.yml --vault-password-file .password --check; \
+	./runsible.py --help;"
 
 if [ "$TRAVIS" == "true" ]; then
 	docker run $REPO /bin/sh -c "$str_TEST"
